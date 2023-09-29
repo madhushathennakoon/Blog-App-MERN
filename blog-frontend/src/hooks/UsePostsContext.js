cbee823afbf96e5c154639b145e3c1c3a942ea9d
Madhusha <madhushasampath@gmail.com>
2023-09-29 20:02:12 +0530

    implement context API

---
+++ blog-frontend/src/hooks/UsePostsContext.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { postContext } from "../context/PostContext";
+
+const UsePostsContext = () => {
+  const context = useContext(postContext);
+
+  if (!context) {
+    throw Error("usePostsContext must be used  inside an postContextProvider");
+  }
+
+  return context;
+};
+
+export default UsePostsContext;
